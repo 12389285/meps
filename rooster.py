078ac8659ce28ea344d9ab5f1861a75cbef52d3b
Max Simons <max_simons@live.nl>
2018-11-07 11:12:07 +0100

    test

--- rooster.py
+++ rooster.py
@@ -9,22 +9,18 @@ def files(INPUT_ZALEN, INPUT_VAKKEN):
 
     # open zalen
     with open(INPUT_ZALEN) as zalen:
-
         zaal_reader = csv.DictReader(zalen)
-
+        # lijst met alle zalen
         zaal_nummers = []
-
         for row in zaal_reader:
             nummer = row['Zaalnummber']
             zaal_nummers.append(nummer)
         print(zaal_nummers)
 
-
     # open Vakken
     with open(INPUT_VAKKEN) as vakken:
-
         vak_reader = csv.DictReader(vakken)
-
+        # lijst met alle vakken
         vak_namen = []
         for row in vak_reader:
             vak = row['Vakken voor periode 4']
