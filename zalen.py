870411011deef8c5130c13fbe4cde70509879bd4
Pascalle Veltman <pascalleveltman@hotmail.nl>
2018-11-08 13:38:51 +0100

    zalen

---
+++ zalen.py
@@ -0,0 +1,11 @@
+# class met alle zalen en zaalgrootte
+
+class zalen()
+    "" Definieer de grootte van iedere zaal"
+    def __init__(self, naam, grootte):
+        self.naam = naam
+        self.grootte = zaalgrootte
+
+    def __str__(self):
+        return f"{self.naam}: {self.grootte}"
+        pass
