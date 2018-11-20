067621d7f9fac3f9bb9a73756cbe6d6815d241fe
Eefje Roelfsema <eefje.roelfsema@gmail.com>
2018-11-20 14:08:19 +0100

    rooster gemaakt

--- rooster.py
+++ rooster.py
@@ -65,9 +65,35 @@ def open_courses(INPUT_COURSES):
                     activity = course_list[i].course_name
                     activity = activity + ' prac' + str(l+1)
                     course_list[i].add(activity)
-            print(course_list[i].activities)
 
-    return course_list
+    # create a queue
+    act_5 = []
+    act_4 = []
+    act_3 = []
+    act_2 = []
+    act_1 = []
+    activities_queue = []
+    for course in course_list:
+        if len(course.activities) == 5:
+            act_5.append(', '.join(str(act) for act in course.activities))
+        elif len(course.activities) == 4:
+            act_4.append(', '.join(str(act) for act in course.activities))
+        elif len(course.activities) == 3:
+            act_3.append(', '.join(str(act) for act in course.activities))
+        elif len(course.activities) == 2:
+            act_2.append(', '.join(str(act) for act in course.activities))
+        elif len(course.activities) == 1:
+            act_1.append(', '.join(str(act) for act in course.activities))
+
+    activities_queue.append(', '.join(str(act) for act in act_5))
+    activities_queue.append(', '.join(str(act) for act in act_4))
+    activities_queue.append(', '.join(str(act) for act in act_3))
+    activities_queue.append(', '.join(str(act) for act in act_2))
+    activities_queue.append(', '.join(str(act) for act in act_1))
+
+    create_schedule(activities_queue)
+
+    return activities_queue
 
 def open_overlapping(INPUT_OVERLAP):
     """
@@ -96,6 +122,31 @@ def open_overlapping(INPUT_OVERLAP):
 
     return overlap_dict
 
+def create_schedule(queue):
+
+    # create empty schedule
+    time_locks = [None] * 5
+    for i in range(4):
+        time_locks[i] = [None] * 7
+    time_locks[4] = [None]
+
+    week = [None] * 5
+    for i in range(5):
+        week[i] = time_locks
+
+    print(week)
+
+    for activity in queue:
+        for day in week:
+            for time_lock in day:
+                for i in range(len(time_lock)):
+                    if time_lock[i] == None:
+                        time_lock[i] = activity
+
+    print(week)
+
+    return(week)
+
 def load(INPUT_ROOM, INPUT_COURSES, INPUT_OVERLAP):
     """
 
@@ -105,6 +156,8 @@ def load(INPUT_ROOM, INPUT_COURSES, INPUT_OVERLAP):
     course_list = open_courses(INPUT_COURSES)
 
     overlap_dict = open_overlapping(INPUT_OVERLAP)
+
+
 
 
 if __name__ == "__main__":
